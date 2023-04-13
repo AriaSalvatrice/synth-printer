fac2088becf06baf5b7cad04b0c0f1bf0b50e46c
Aria Salvatrice <woof@aria.dog>
2023-04-13 20:36:06 +0200

    Better test panel for demo

--- testpanel.py
+++ testpanel.py
@@ -1,20 +1,30 @@
 import cadquery as cq
 from synthprinter import *
 
-sp = SynthPrinter()
+# You can override any setting from the defaultConfig
+sp = SynthPrinter(
+    tolerance=0.4,
+    panelColorRGBA=cq.Color(0.5, 0.9, 0.5, 0.9),
+    miniToggleSwitchDiameter=6,
+)
 
-sp.addEurorackPanel(8)
+sp.addEurorackPanel(8)  # HP
 
 sp.addLed5mm(hp(1), 10)
 sp.addMiniToggleSwitch(hp(1), 20, "vertical")
 sp.addLed3mm(hp(1), 30)
+sp.addMiniToggleSwitch(hp(1), 40, "vertical")
 
 sp.addPotentiometer(hp(4), 20)
 
-sp.addLed5mm(hp(7), 10)
-sp.addLed5mm(hp(7), 20)
-sp.addLed5mm(hp(7), 30)
-sp.addLed5mm(hp(7), 40)
+sp.addLed5mm(32, 10)
+sp.addLed5mm(37, 15)
+sp.addLed5mm(32, 20)
+sp.addLed5mm(37, 25)
+sp.addLed5mm(32, 30)
+sp.addLed5mm(37, 35)
+sp.addLed5mm(32, 40)
+sp.addLed5mm(37, 45)
 
 sp.addMiniToggleSwitch(hp(4), 40)
 
